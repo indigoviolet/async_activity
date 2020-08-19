be6e60e78aa63d58b67891ff668ddd3b3939041e
Venky Iyer <indigoviolet@gmail.com>
2020-08-18 18:02:44 -0700

    relative imports; fix mypy VIRTUAL_ENV

--- async_activity/__init__.py
+++ async_activity/__init__.py
@@ -1,3 +1,3 @@
-from activity_queue import (ActivityMonitor, ActivityQueue, async_q_tee,
-                            log_activity, log_inactivity)
-from aiopynput import AioPynput
+from .activity_queue import (ActivityMonitor, ActivityQueue, async_q_tee,
+                             log_activity, log_inactivity)
+from .aiopynput import AioPynput
